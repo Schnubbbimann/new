48611c82710af63b5cffd635f145f5391aba8000
Schnubbbimann <felix.lommel@online.de>
2026-03-09 11:25:45 +0100

    Update roomManager.js

--- server/roomManager.js
+++ server/roomManager.js
@@ -1,25 +1,41 @@
+// server/roomManager.js
 const CaboGame = require("./gameEngine");
 
 class RoomManager {
   constructor() {
+    // rooms: { [roomId]: { players: [socketId], names: { socketId: name }, game: CaboGame|null } }
     this.rooms = {};
   }
 
   createRoom(id) {
     this.rooms[id] = {
       players: [],
+      names: {},
       game: null,
     };
   }
 
-  joinRoom(id, player) {
-    if (!this.rooms[id]) return false;
-    this.rooms[id].players.push(player);
+  joinRoom(id, socketId, name = "Spieler") {
+    const room = this.rooms[id];
+    if (!room) return false;
+    if (!room.players.includes(socketId)) {
+      room.players.push(socketId);
+      room.names[socketId] = name;
+    }
     return true;
+  }
+
+  leaveRoom(id, socketId) {
+    const room = this.rooms[id];
+    if (!room) return;
+    room.players = room.players.filter(p => p !== socketId);
+    delete room.names[socketId];
+    if (room.players.length === 0) delete this.rooms[id];
   }
 
   startGame(id) {
     const room = this.rooms[id];
+    if (!room) return null;
     room.game = new CaboGame(room.players);
     return room.game;
   }
